d7e427f4ccce82e23abbb021d4b1f3020c71c3cd
YukioYoko <yukio.yokogawa141002@gmail.com>
2026-03-02 12:54:59 -0600

    cart in cookies 5

--- hooks/useCarrito.ts
+++ hooks/useCarrito.ts
@@ -2,7 +2,7 @@
 import { useState, useTransition, useEffect, useRef } from "react";
 import Cookies from "js-cookie";
 import { io, Socket } from "socket.io-client";
-import { sendOrder } from "@/app/menu/action"; 
+import { sendOrder } from "@/app/(clientView)/menu/action"; 
 import { getSugerenciasApriori } from "@/components/products/action";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001";
